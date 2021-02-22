89f85475f4267bb2209436b9af48929e4d457f14
devansh5 <devkumar5436@gmail.com>
2021-02-22 13:34:50 +0530

    generating random article

--- parse_sitemap_xml.js
+++ parse_sitemap_xml.js
@@ -1,24 +1,23 @@
 const Sitemapper = require('sitemapper');
-const express=require('express');
-const app=express()
+const express = require('express');
+const app = express()
 const sitemap = new Sitemapper();
 
-let a,b;
 
-app.get('/random',(req,res)=>{
-   sitemap.fetch('https://iq.opengenus.org/sitemap-posts.xml').then(function(response) {
-   a=randomIntFromInterval(0,response.sites.length)
-   b=response.sites[a]
-   res.status(301).redirect(b)
-})
-  
+app.get('/random', (req, res) => {
+   sitemap.fetch('https://iq.opengenus.org/sitemap-posts.xml').then(function (response) {
+      let randomIndexUrl = randomIntFromInterval(0, response.sites.length)
+      let randomURL = response.sites[randomIndexUrl]
+      res.status(301).redirect(randomURL)
+   })
+
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
    console.log("running")
 })
 
 
-function randomIntFromInterval(min, max) { 
+function randomIntFromInterval(min, max) {
    return Math.floor(Math.random() * (max - min + 1) + min);
- }
+}
